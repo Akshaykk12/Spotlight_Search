22f0ad39f2c33c3f695cfcf1af0be4ee16aa153e
Akshay Kudalkar <105927588+Akshaykk12@users.noreply.github.com>
2023-05-20 23:58:05 +0530

    Made GUI
    GUI looks like the spotlight search bar with a little glass effect

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace GlassSearchBar
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow = this; // Set the main window
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Application.Current.Shutdown(); // Shutdown the application when the window is closed
+        }
+    }
+}
